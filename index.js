f20e18185cc44a35ee651999305337ee5fc6db3d
Wojciech Wichary <wojciechwichary.biz@gmail.com>
2020-07-06 21:47:23 +0200

    Fix scroll

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-const navList = document.querySelector('.nav-list');
+document.addEventListener('DOMContentLoaded', function () {
 
-const navLinks = document.querySelectorAll('.dot');
 
-for (let i = 0; i < navLinks.length; i++) {
-    navLinks[i].addEventListener("click", function () {
-        let current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-    });
-}
+    const rootElement = document.querySelector('#root');
+    const sections = document.querySelectorAll('section');
+    let currentSectionIndex = 0;
+    let isTrottled = false;
+
+
+    document.addEventListener('mousewheel', function (event) {
+        if (isTrottled) return;
+        isTrottled = true;
+        setTimeout(function () {
+            isTrottled = false;
+        }, 1000);
+        const direction = event.wheelDelta < 0 ? 1 : -1;
+        scroll(direction);
+    })
+
+    function scroll(direction) {
+        if (direction === 1) {
+            const isLastSection = currentSectionIndex === sections.length - 1;
+            if (isLastSection) return;
+        } else if (direction === -1) {
+            const isFirstSection = currentSectionIndex === 0;
+            if (isFirstSection) return;
+        }
+
+        currentSectionIndex = currentSectionIndex + direction;
+
+        scrollToCurrentSection();
+    }
+
+    function scrollToCurrentSection() {
+        sections[currentSectionIndex].scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        })
+    }
+})
