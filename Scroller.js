7cab6b9df4ab093599a1aa204586f647e9521a0b
Wojciech Wichary <wojciechwichary.biz@gmail.com>
2020-07-06 23:24:30 +0200

    Add section titles to nav bars

--- Scroller.js
+++ Scroller.js
@@ -28,7 +28,7 @@ class Scroller {
 
         setTimeout(() => {
             this.isThrottled = false;
-        }, 1000);
+        }, 500);
 
         const direction = event.deltaY > 0 ? 1 : -1;
 
@@ -59,28 +59,92 @@ class Scroller {
 
     }
 
+    // drawNavigation() {
+    //     this.navigationContainer = document.createElement('aside');
+    //     this.navigationContainer.setAttribute('class', 'scroller-navigation');
+    //     const list = document.createElement('ul');
+    //     list.setAttribute('class', 'scroller-navigation__list')
+    //     this.sections.forEach((section, index) => {
+    //         const listItem = document.createElement('li');
+    //         listItem.setAttribute('class', 'scroller-navigation__element')
+    //         listItem.innerHTML = `<div class="dot-box">
+    //                 <a class="dot"></a>
+    //             </div>
+    //             <a class="nav-link">fifth</a>`
+
+    //         listItem.addEventListener('click', () => {
+    //             this.currentSectionIndex = index;
+    //             this.scrollToCurrentSection()
+    //         })
+
+    //         list.appendChild(listItem);
+    //     })
+    //     this.navigationContainer.appendChild(list);
+
+    //     document.body.appendChild(this.navigationContainer);
+
+    //     this.selectActiveNavItem();
+    // }
     drawNavigation() {
         this.navigationContainer = document.createElement('aside');
         this.navigationContainer.setAttribute('class', 'scroller-navigation');
-        const list = document.createElement('ul');
-        list.setAttribute('class', 'scroller-navigation__list')
-        this.sections.forEach((section, index) => {
-            const listItem = document.createElement('li');
-            listItem.setAttribute('class', 'scroller-navigation__element')
-            listItem.innerHTML = `<div class="dot-box">
+
+        this.navigationContainer.innerHTML = `
+        <ul class = "scroller-navigation__list">
+            <li class="scroller-navigation__element">
+                <div class="dot-box">
                     <a class="dot"></a>
-                </div>`
-
-            listItem.addEventListener('click', () => {
-                this.currentSectionIndex = index;
-                this.scrollToCurrentSection()
-            })
-
-            list.appendChild(listItem);
-        })
-        this.navigationContainer.appendChild(list);
-
+                </div>
+                <a class="nav-link">first</a>
+            </li>
+            <li class="scroller-navigation__element">
+                <div class="dot-box">
+                    <a class="dot" ></a>
+                </div>
+                <a class="nav-link">second</a>
+            </li>
+            <li class="scroller-navigation__element">
+                <div class="dot-box">
+                    <a class="dot "></a>
+                </div>
+                <a class="nav-link">thirth</a>
+            </li>
+            <li class="scroller-navigation__element">
+                <div class="dot-box">
+                    <a class="dot active"></a>
+                </div>
+                <a class="nav-link">fourth</a>
+            </li>
+            <li class="scroller-navigation__element">
+                <div class="dot-box">
+                    <a class="dot"></a>
+                </div>
+                <a class="nav-link">fifth</a>
+            </li>
+            <li class="scroller-navigation__element">
+                <div class="dot-box">
+                    <a class="dot"></a>
+                </div>
+                <a class="nav-link">sixth</a>
+            </li>
+            <li class="scroller-navigation__element">
+                <div class="dot-box">
+                    <a class="dot"></a>
+                </div>
+                <a class="nav-link">seventh</a>
+            </li>
+        </ul>
+        `
         document.body.appendChild(this.navigationContainer);
+
+
+        const listItems = document.querySelectorAll('li');
+        listItems.forEach((item, index) => {
+            item.addEventListener('click', () => {
+                this.currentSectionIndex = index;
+                this.scrollToCurrentSection();
+            })
+        })
 
         this.selectActiveNavItem();
     }
@@ -88,7 +152,7 @@ class Scroller {
     selectActiveNavItem() {
         if (this.navigationContainer) {
 
-            const navigationItems = this.navigationContainer.querySelectorAll('a');
+            const navigationItems = this.navigationContainer.querySelectorAll('a.dot');
 
             navigationItems.forEach((item, index) => {
                 if (index === this.currentSectionIndex) {
